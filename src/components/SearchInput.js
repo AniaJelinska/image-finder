abf121c8d40a81922a18c4b413036ca2611a77c9
Ania Jelińska <ania.jelin@wp.pl>
2020-11-06 09:26:52 +0100

    Change to a function component. Fetch the data.

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,47 +1,53 @@
-import React, { Component } from 'react';
+
+import React, { useState } from "react";
 import Unsplash, { toJson } from "unsplash-js";
 
 const unsplash = new Unsplash({
     accessKey: "PUc2mcO-lkccQgrnP0cU25N6Gu-QP6q5mB4QrQF8GFo",
 });
 
-class SearchInput extends Component {
-    state = {
-        keyword: "",
-    }
-    handleChange = (e) => {
-        const value = e.target.value
-        this.setState({
-            keyword: value
-        });
-    };
+function SearchInput() {
+    const [keyword, setKeyword] = useState("");
+    const [images, setImages] = useState([]);
 
-    handleSearchImages = async (e) => {
+
+    const handleSearchImages = async (e) => {
         e.preventDefault();
         unsplash.search
-            .photos(this.state.keyword)
+            .photos(keyword)
             .then(toJson)
             .then((json) => {
-                console.log(json);
+                setImages(json.results);
             });
     }
-
-    render() {
-        return (
+    return (
+        <>
             <div className="formContainer">
-                <form className="form" onSubmit={this.handleSearchImages}>
+                <form className="form" onSubmit={handleSearchImages}>
                     <input
                         className="input"
                         type="text"
                         placeholder={"Search free high-resolution photos"}
-                        value={this.state.keyword}
-                        onChange={this.handleChange}
+                        value={keyword}
+                        onChange={(e) => setKeyword(e.target.value)}
                     >
                     </input>
                 </form>
+                <div className="imgList">
+                    {images.map((image) => <div className="card" key={image.id}>
+                        <img
+                            className="cardImage"
+                            alt={image.alt_description}
+                            src={image.urls.full}
+                            width="50%"
+                            height="50%"
+                        ></img>
+                    </div>)
+                    }
+                </div>
             </div>
-        )
-    }
+        </>
+    )
 }
 
 export default SearchInput;
