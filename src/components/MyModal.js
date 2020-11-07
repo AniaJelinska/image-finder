8b8601180aeb273e5643251f06af0cd2b350a2c0
Ania Jelińska <ania.jelin@wp.pl>
2020-11-07 15:57:03 +0100

    create modal component - WIP

---
+++ src/components/MyModal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Modal, Image } from "react-bootstrap";
+
+function MyModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    <Image src={props.profileImage} roundedCircle />
+                    {props.name}
+                    <br />
+                    @{props.username}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <img src={props.data} />
+            </Modal.Body>
+            <Modal.Footer>
+                {props.location}
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default MyModal;
